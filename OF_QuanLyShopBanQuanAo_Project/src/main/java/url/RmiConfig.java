32cff12a42e3df2d8eb6d3234d0268529f69dd1c
ChienHandsome <cerkvena291@gmail.com>
2025-01-18 10:48:34 +0700

    Change classes's location

---
+++ OF_QuanLyShopBanQuanAo_Project/src/main/java/url/RmiConfig.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package url;
+
+/**
+ *
+ * @author TriHieu
+ */
+public class RmiConfig {
+    public static final String RMI_URL = "rmi://10.0.0.90:7878/";
+}
